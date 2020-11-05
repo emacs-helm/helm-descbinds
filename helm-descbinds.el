52d94c1e5fd603db650c8cf8fe1f237df661a1f5
Przemyslaw Kryger <pkryger@gmail.com>
2020-11-05 11:45:50 +0000

    Use `helm-describe-function-function` for a help
    This allow to use the same function to describe function as other helm functions
    , i.e. `helm-M-x`.
    
    I find it useful to use with `helpful` package, while retaining existing functionality.

--- helm-descbinds.el
+++ helm-descbinds.el
@@ -224,7 +224,7 @@ see (info \"(elisp) Prefix Keys\").")
        (helm-descbinds-display-string-in-help
         helm-descbinds-prefix-help))
       ((guard (and (symbolp name) (fboundp name)))
-       (describe-function name)))))
+       (funcall helm-describe-function-function name)))))
 
 (defun helm-descbinds-action:find-func (candidate)
   "An action that find selected CANDIDATE function."
